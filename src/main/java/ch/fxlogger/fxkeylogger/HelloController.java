6c6b491f65e3a20d48db462a0d34f73738a0bbd7
Dragxfly <59048803+JannisJost@users.noreply.github.com>
2022-02-23 10:24:21 +0100

    added controller for main view

--- src/main/java/ch/fxlogger/fxkeylogger/HelloController.java
+++ src/main/java/ch/fxlogger/fxkeylogger/HelloController.java
@@ -1,15 +1,40 @@
 package ch.fxlogger.fxkeylogger;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 
-public class HelloController {
+public class HelloController implements Initializable {
+
     @FXML
-    private Label welcomeText;
-//hello
-    
+    private Button btnStartOrStopLogging;
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private ChoiceBox<String> choiceLogType;
+    @FXML
+    private Label lblActivityIndicator;
+    @FXML
+    private ImageView btnCloseWindow;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        choiceLogType.getItems().add("Database");
+        choiceLogType.getItems().add("File");
+        choiceLogType.setValue("Database");
+    }
+
+    @FXML
+    private void startOrStopLogging(ActionEvent event) {
+    }
+
+    @FXML
+    private void closeWindow(MouseEvent event) {
+        System.exit(0);
     }
 }
