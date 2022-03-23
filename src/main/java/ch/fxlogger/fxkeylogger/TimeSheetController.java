d138500ac2999ea391c58cfe0ec64bbd26870a2e
Damian-Sommer <69591092+Damian-Sommer@users.noreply.github.com>
2022-03-23 08:51:30 +0100

    Update TimeSheetController.java

--- src/main/java/ch/fxlogger/fxkeylogger/TimeSheetController.java
+++ src/main/java/ch/fxlogger/fxkeylogger/TimeSheetController.java
@@ -1,12 +1,21 @@
 package ch.fxlogger.fxkeylogger;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TableView;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class TimeSheetController implements Initializable {
+    @FXML
+    private ImageView btnCloseWindow;
+
+    @FXML
+    private TableView<?> table;
+
+
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
